a62d4b24b2a481177ba4a65be7bb302f75a2c474
monicachacon25 <118320747+monicachacon25@users.noreply.github.com>
2024-05-13 12:55:24 -0600

    inicio

---
+++ mixins/menuData.js
@@ -0,0 +1,43 @@
+export default {
+  data() {
+    return {
+      menuData: [
+        {
+          id: 1,
+  
+    title: "U5",
+          link: "/",
+        },
+        {
+          id: 2,
+          title: "U6",
+          megaMenu: true,
+          link: "about",
+  
+        },
+        {
+          id: 3,
+
+          title: "Servicio",
+          link: "/portfolio",
+        
+        },
+        {
+          id: 4,
+   
+          title: "Donde estamos",
+          link: "/blog",
+       
+        },
+      
+        {
+          id: 6,
+        
+          title: "Acerca de",
+          link: "/contact",
+          
+        },
+      ],
+    };
+  },
+};
