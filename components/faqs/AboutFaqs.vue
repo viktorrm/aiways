f1cfd917e662cdfada02e6e7746305b83487184a
viktorrm <victibun@gmail.com>
2024-05-15 15:53:47 -0600

    SPECS titulo fix

--- components/faqs/AboutFaqs.vue
+++ components/faqs/AboutFaqs.vue
@@ -1,27 +1,21 @@
 <template>
-  <section class="faq__area p-relative">
-    <div class="container">
+  <section class="faq__area">
+    <div class="container pb-25">
       <div class="">
-    <div class=" text-center "> <h1 class="centrar">Tren de potencia y paquete de baterías</h1></div>
+        <div class="text-center">
+          <h1 class="centrar">Tren de potencia y paquete de baterías</h1>
+        </div>
+      </div>
     </div>
-    </div>
-    <div class="faq__video" :style="{ backgroundImage: `url(${bg})` }">
-   <!--  -->
-    </div>
-    <div class="container-fluid">
-      <div class="row justify-content-end">
-        <div class="col-xxl- col-xl-7 col-lg-7">
-          <div
-            class="faq__wrapper-2  faq__style-2 tp-accordion pl-160"
-          >
-           <!--  <div class="faq__title-wrapper">
-              <span class="faq__title-pre"
-                >{{el_style ? 'Minimal Design':'Tren de potencia y paquete de baterías.'}}</span
-              >
-              <h3 class="faq__title">
-                {{el_style ? 'Accordion with image':''}}
-              </h3>
-            </div> -->
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+          <img :src="bg" alt="" class="w-100" />
+        </div>
+
+        <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+          <div class="faq__wrapper-2 faq__style-2 tp-accordion">
             <div class="accordion" id="about-faqaccordion">
               <single-faq
                 v-for="(item, i) in faq_items"
@@ -33,14 +27,18 @@
         </div>
       </div>
     </div>
-
   </section>
-<div class="container">
-  <div class="row">
-    <div> <h4 class="">Aiways U5: Consumo combinado de electricidad¹ en kWh/100 km: 13.8 (NEDC);
-
-17.0–16.6 (WLTP). Emisiones combinadas de CO₂¹: 0 g/km. Clase de eficiencia: A+</h4></div></div></div>
-
+  <div class="container">
+    <div class="row">
+      <div>
+        <h4 class="">
+          Aiways U5: Consumo combinado de electricidad¹ en kWh/100 km: 13.8
+          (NEDC); 17.0–16.6 (WLTP). Emisiones combinadas de CO₂¹: 0 g/km. Clase
+          de eficiencia: A+
+        </h4>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -50,11 +48,11 @@ import VideoModal from "../common/modals/VideoModal.vue";
 
 export default {
   components: { SingleFaq, VideoModal },
-  props:{
-    el_style:{
-      type:Boolean,
-      default:false,
-    }
+  props: {
+    el_style: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
@@ -79,13 +77,13 @@ export default {
           desc: "",
           parent: "about-faqaccordion",
         },
-         {
+        {
           id: "about-three",
           title: "Seguridad",
           desc: "La separación entre las zonas secas y húmedas garantiza un nivel de seguridad sin precedentes, incluso si la batería se deforma o se rompe. En caso de una colisión grave, el módulo de la batería también está protegido contra fugas de refrigerante. Esto elimina el riesgo de incendio/explosión. Como tal, un espacio de 15 mm entre la parte inferior de la placa de la batería y el módulo de la batería sirve como protección contra impactos",
           parent: "about-faqaccordion",
         },
-         {
+        {
           id: "about-three",
           title: "Enfriamiento",
           desc: "",
@@ -102,11 +100,9 @@ export default {
 };
 </script>
 <style scoped>
-.faq__style-2{
+.faq__style-2 {
   border-left: none;
+  padding-right: 0px !important;
+  padding-top: 0px !important;
 }
-.centrar{
-  position: absolute;
-}
-
 </style>
