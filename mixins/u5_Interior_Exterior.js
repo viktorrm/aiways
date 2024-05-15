cdd2a77610d63cc6db7b8aeac829c3ba5ad26845
monicachacon25 <118320747+monicachacon25@users.noreply.github.com>
2024-05-15 16:20:12 -0600

    cambios tarde

--- mixins/u5_Interior_Exterior.js
+++ mixins/u5_Interior_Exterior.js
@@ -3,15 +3,63 @@ import spec2 from "~/assets/img/aiways/carsSpecs/u5/spec2.jpg";
 import exteriorruedas from "~/assets/img/aiways/carsSpecs/u5/exteriorruedas.jpg";
 import exteriortecho from "~/assets/img/aiways/carsSpecs/u5/exteriortecho.jpg";
 import exteriorpuerta from "~/assets/img/aiways/carsSpecs/u5/exteriorpuerta.jpg";
+import interiorasientos from "~/assets/img/aiways/carsSpecs/U5/interiorasientos.jpg";
+import interiorpito from "~/assets/img/aiways/carsSpecs/U5/interiorpito.jpg";
+import interiortouchscreen from "~/assets/img/aiways/carsSpecs/U5/interiortouchscreen.jpg";
+import interiorvistatecho from "~/assets/img/aiways/carsSpecs/U5/interiorvistatecho.jpg";
+import interiorespacioguardado from "~/assets/img/aiways/carsSpecs/U5/interiorespacioguardado.jpg";
+import aire from "~/assets/img/aiways/carsSpecs/U5/aire.jpg";
+
 
 export const specs = {
   interior: [
-    {
-      title: "Comfortable seats",
+     {
+      title: "Asientos comfortables",
       desc: {
-        titulo: "Safe and comfortable",
-        desc: "All passengers in the PRIME version of the U5 sit on a leather/imitation leather combination that ensures stable hold from all sides.The driver can look forward to a seat that is adjustable in six directions (four directions in the case of the front passenger seat), not to mention a lumbar support that is electronically adjustable in two directions. Upholstered central armrests provide even more comfort, in both the front and back. The back seats can be folded in a 60 : 40 split. By raising the load floor, it is possible to create a flat compartment with a capacity of 1,543 litres. And with the I-SIZE child seat attachment units, the U5 offers extra safety for the little ones.",
+        titulo: "Seguro y cómodo",
+        desc: "Todos los pasajeros en la versión PRIME del U5 se sientan en una combinación de cuero/imitación de cuero que asegura un soporte estable desde todos los lados. El conductor puede esperar un asiento ajustable en seis direcciones (cuatro direcciones en el caso del asiento del pasajero delantero), sin mencionar un soporte lumbar ajustable electrónicamente en dos direcciones. Los reposabrazos centrales acolchados proporcionan aún más comodidad, tanto en la parte delantera como en la trasera. Los asientos traseros se pueden plegar en una división de 60 : 40. Al elevar el piso de carga, es posible crear un compartimento plano con una capacidad de 1,543 litros. Y con las unidades de sujeción de sillas de auto I-SIZE, el U5 ofrece seguridad adicional para los más pequeños.",
       },
+      img: interiorasientos,
+    },
+    {
+      title: "Cockpit",
+      desc: {
+        titulo: "Todo bajo control",
+        desc: "El futuro ha llegado al cockpit del U5.Una pantalla combinada de tres partes 4.2 / 7 / 4.2 pulgadas inclinada hacia el conductor, proporciona un resumen digital de toda la información clave del vehículo, como el estado de carga de la batería y el frenado regenerativo sin problemas. Y como el volante, revestido de cuero de microfibra, ha sido nivelado en la parte superior e inferior, el conductor tiene una vista despejada del panel de instrumentos futurista en todo momento. La altura y la profundidad del volante también se pueden ajustar libremente según sus necesidades./ ",
+      },
+      img: interiorpito,
+    },
+    {
+      title: "Pantalla táctil central",
+      desc: {
+        titulo: "Cockpit minimalista, control máximoe",
+        desc: "El eje de tu nuevo centro de control: la pantalla táctil LCD suspendida de 12.3 pulgadas (1,920x720 dpi) con protección antirreflejos, antirreflejos y anti manchas. Permite un acceso inmediato e intuitivo al entretenimiento, la climatización automática, el techo panorámico y mucho más. Como tal, interruptores, botones y palancas confusos y antiestéticos quedan relegados al pasado. Los teléfonos inteligentes se pueden conectar fácilmente a través de Apple CarPlay y operarse mediante la pantalla táctil. Además, también puede acceder a actualizaciones y mejoras regulares a través de la pantalla táctil central.",
+      },
+      img:interiortouchscreen,
+    },
+    {
+      title: "Techo panorámico",
+      desc: {
+        titulo: "Sensación de convertible",
+        desc: "La versión PRIME del U5 ofrece el techo panorámico más grande de su clase con bastante margen. La luz entra por nada menos que dos tercios del techo, haciendo que el interior ya espacioso parezca aún más grande. ¡Mira hacia arriba y disfruta de la vista espectacular! Y no olvides tu protector solar.",
+      },
+      img: interiorvistatecho,
+    },
+    {
+      title: "Espacio de almacenamiento",
+      desc: {
+        titulo: "Lo encontrarás en todas partes: espacio.",
+        desc: "No te faltará nada en el Aiways U5. Especialmente espacio. Ya sea para cosas pequeñas o grandes: gracias al práctico espacio de almacenamiento en y debajo de la consola central, en las puertas y en el compartimento para guantes opcional, todo se puede guardar fácilmente y encontrar en un abrir y cerrar de ojos.Gracias a una capacidad de maletero de 496 litros cuando el respaldo está erguido y 1,619 litros cuando está plegado, puedes llevar todo contigo de vacaciones. Y si eso no es suficiente, hay espacio adicional debajo del piso de carga en el maletero. Ni siquiera tienes que usar este espacio para los cables de carga, ya que se almacenan al alcance de la mano debajo del capó del motor.",
+      },
+      img: interiorespacioguardado
+    },
+    {
+      title: "Aire acondicionado",
+      desc: {
+        titulo: "Aire acondicionado de doble zona",
+        desc: "Gracias al aire acondicionado automático de doble zona, tanto el conductor como el pasajero delantero pueden disfrutar de la temperatura deseada, que se puede configurar fácilmente a través de la pantalla táctil central. También hay un ventilador de aire acondicionado disponible para ajustar la temperatura de los pasajeros en el asiento trasero. Y si te encuentras en un atasco, puedes dejar el aire acondicionado automático funcionando, incluso cuando el motor está apagado.",
+      },
+      img: aire,
     },
   ],
 
