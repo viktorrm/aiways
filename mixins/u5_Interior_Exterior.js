42af1d8859eb09fd9cf061e324610229c12be92d
monicachacon25 <118320747+monicachacon25@users.noreply.github.com>
2024-05-15 12:58:48 -0600

    interior

--- mixins/u5_Interior_Exterior.js
+++ mixins/u5_Interior_Exterior.js
@@ -4,26 +4,36 @@ import spec2 from "~/assets/img/aiways/carsSpecs/u5/spec2.jpg";
 export const specs = {
   interior: [
     {
-      title: "Design",
+      title: "Comfortable seats",
       desc: {
-        titulo: "Proof that big can be beautiful",
-        desc: "4.68 m long, 1.87 m wide, 1.7 m high. This five-door car is not only a treat for the eyes on account of its pure size and streamlined silhouette, but also thanks to sophisticated details such as recessed door handles and an aerodynamic spoiler – both of which serve to reduce drag. What's more, the charging connector below the left-hand headlamp is not only easily accessible, but also exceptionally well concealed.",
+        titulo: "Safe and comfortable",
+        desc: "All passengers in the PRIME version of the U5 sit on a leather/imitation leather combination that ensures stable hold from all sides.The driver can look forward to a seat that is adjustable in six directions (four directions in the case of the front passenger seat), not to mention a lumbar support that is electronically adjustable in two directions. Upholstered central armrests provide even more comfort, in both the front and back. The back seats can be folded in a 60 : 40 split. By raising the load floor, it is possible to create a flat compartment with a capacity of 1,543 litres. And with the I-SIZE child seat attachment units, the U5 offers extra safety for the little ones.",
+      },
+    },
+  ],
+  exterior: 
+  [
+    {
+      title: "Diseño",
+      desc: {
+        titulo: "La prueba de que lo grande puede ser hermoso.",
+        desc: "Con 4.68 metros de largo, 1.87 metros de ancho y 1.7 metros de alto, este automóvil de cinco puertas no solo es un festín para los ojos debido a su tamaño imponente y su silueta aerodinámica, sino también gracias a detalles sofisticados como los tiradores de las puertas empotrados y un alerón aerodinámico, ambos diseñados para reducir la resistencia al aire. Además, el conector de carga debajo del faro izquierdo no solo es fácilmente accesible, sino que también está excepcionalmente bien oculto.",
       },
       img: spec1,
     },
     {
       title: "Design",
       desc: {
-        titulo: "Light in the spotlight",
-        desc: "The L-shaped LED headlights and the LED tail lights, which are integrated across the entire rear, are eye-catchers in their own right. Thanks to their unique multi layered design, they blend seamlessly into the vehicle exterior.",
-      },
+        titulo: "Luz en el centro de atención",
+        desc: "Los faros LED con forma de L y las luces traseras LED, que están integradas en toda la parte trasera, son atractivos por sí mismos. Gracias a su diseño único de múltiples capas, se integran perfectamente en el exterior del vehículo.",
+      }
       img: spec2,
     },
     {
       title: "Wheels",
       desc: {
-        titulo: "Alloy wheels",
-        desc: 'In the XCITE configuration, 17" alloy wheels bridge the gap between the car and the road; the PRIME version boasts two-tone 19" alloy wheels with aerodynamically optimised tyres.',
+        titulo: "Llantas de aleación",
+        desc: 'En la configuración XCITE, las llantas de aleación de 17" llenan el espacio entre el automóvil y la carretera; la versión PRIME cuenta con llantas de aleación de dos tonos de 19" con neumáticos optimizados aerodinámicamente.',
       },
       img: spec1,
     },
@@ -42,15 +52,6 @@ export const specs = {
         desc: "In the PRIME version, the electric boot lid opens at the push of a button; it is even contactless and can be activated with a simple foot movement beneath the rear apron. As there is no loading sill, loading/unloading is easier and more straightforward than ever before.",
       },
       img: spec1,
-    },
-  ],
-  exterior: [
-    {
-      title: "Comfortable seats",
-      desc: {
-        titulo: "Safe and comfortable",
-        desc: "All passengers in the PRIME version of the U5 sit on a leather/imitation leather combination that ensures stable hold from all sides.The driver can look forward to a seat that is adjustable in six directions (four directions in the case of the front passenger seat), not to mention a lumbar support that is electronically adjustable in two directions. Upholstered central armrests provide even more comfort, in both the front and back. The back seats can be folded in a 60 : 40 split. By raising the load floor, it is possible to create a flat compartment with a capacity of 1,543 litres. And with the I-SIZE child seat attachment units, the U5 offers extra safety for the little ones.",
-      },
     },
   ],
 };
