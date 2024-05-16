4411e697844dc7715cce63781b33165a49f83406
monicachacon25 <118320747+monicachacon25@users.noreply.github.com>
2024-05-16 11:50:52 -0600

    cambios2

--- pages/AcercaDe.vue
+++ pages/AcercaDe.vue
@@ -1,36 +1,19 @@
 <template>
   <LayoutOne>
-    <HomeHero />
-  <!--   <HomeAbout/> -->
-    <AboutTextArea2/>
-    <BoxShadowStyle/>
-     <ElVideoMain/>
-      <ElVideoMain/>
-      <AgencyPricing/>
-      <!-- <ElVideoMain/> -->
-     <!--  <DetailsDesc/> -->
-     <SliderWithDots/>
-     <AboutFaqs/>
-<FaqArea/>
-<HomeAbout/>
-<AgencyPricing/>
-<EspecificacionesTec2/>
+<serviciosinfo/>
+
+ 
+
   </LayoutOne>
 </template>
 
 <script setup>
 import LayoutOne from '~/layouts/LayoutOne.vue';
-import HomeHero from '~/components/hero-banner/HomeHero.vue';
-import AboutTextArea2 from "/components/Aiways/AboutTextArea2.vue";
-import BoxShadowStyle from '~~/elements/elements-iconbox/BoxShadowStyle.vue';
-import ElVideoMain from '~~/elements/elements-video/ElVideoMain.vue';
-import SliderWithDots from '~~/elements/elements-carousel-main/SliderWithDots.vue';
+import serviciosinfo from "/components/Aiways/serviciosinfo.vue";
 import DetailsDesc from '~~/components/product-details/DetailsDesc.vue';
-import AboutFaqs from '~~/components/faqs/AboutFaqs.vue';
-import FaqArea from '~~/components/faqs/FaqArea.vue';
-import AgencyPricing from '~~/components/pricing/AgencyPricing.vue';
-import HomeAbout from '~/components/abouts/HomeAbout.vue';
-import EspecificacionesTec2 from '~~/components/Aiways/EspecificacionesTec2.vue';
+
+
+
 
 useHead({
   title: "Aiways Costa Rica",
