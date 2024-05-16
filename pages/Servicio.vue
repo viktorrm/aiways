17f6aea5c63327c221ec4ef00b8424d46095b56e
monicachacon25 <118320747+monicachacon25@users.noreply.github.com>
2024-05-15 21:46:19 -0600

    cambios finales

--- pages/Servicio.vue
+++ pages/Servicio.vue
@@ -1,36 +1,28 @@
 <template>
   <LayoutOne>
-    <HomeHero />
-  <!--   <HomeAbout/> -->
+   
+<ElementsHeadingMain/>
     <AboutTextArea2/>
     <BoxShadowStyle/>
-     <ElVideoMain/>
-      <ElVideoMain/>
-      <AgencyPricing/>
-      <!-- <ElVideoMain/> -->
-     <!--  <DetailsDesc/> -->
-     <SliderWithDots/>
-     <AboutFaqs/>
-<FaqArea/>
-<HomeAbout/>
-<AgencyPricing/>
-<EspecificacionesTec2/>
-  </LayoutOne>
+    <HomeAbout/>
+<UnderlineAccordion/>
+</LayoutOne>
 </template>
 
 <script setup>
 import LayoutOne from '~/layouts/LayoutOne.vue';
-import HomeHero from '~/components/hero-banner/HomeHero.vue';
+
+import UnderlineAccordion from '~~/elements/elements-accordion/UnderlineAccordion.vue'
+import ElementsHeadingMain from '~~/elements/elements-heading/ElementsHeadingMain.vue'
 import AboutTextArea2 from "/components/Aiways/AboutTextArea2.vue";
 import BoxShadowStyle from '~~/elements/elements-iconbox/BoxShadowStyle.vue';
-import ElVideoMain from '~~/elements/elements-video/ElVideoMain.vue';
-import SliderWithDots from '~~/elements/elements-carousel-main/SliderWithDots.vue';
-import DetailsDesc from '~~/components/product-details/DetailsDesc.vue';
+
 import AboutFaqs from '~~/components/faqs/AboutFaqs.vue';
 import FaqArea from '~~/components/faqs/FaqArea.vue';
 import AgencyPricing from '~~/components/pricing/AgencyPricing.vue';
 import HomeAbout from '~/components/abouts/HomeAbout.vue';
 import EspecificacionesTec2 from '~~/components/Aiways/EspecificacionesTec2.vue';
+import ElementsHeadingMainVue from '~~/elements/elements-heading/ElementsHeadingMain.vue';
 
 useHead({
   title: "Aiways Costa Rica",
