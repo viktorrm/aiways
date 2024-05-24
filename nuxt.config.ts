9c64e0d3e9f612e76fe9c88c76d216914a215909
viktorrm <victibun@gmail.com>
2024-05-24 15:17:44 -0600

    package json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  target: "static",
   css: [
     "bootstrap/scss/bootstrap.scss",
     "swiper/css/bundle",
